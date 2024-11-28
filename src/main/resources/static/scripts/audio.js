f714f8414f7337b054c93f55f5f3c10e0be15c02
tornadotang <tangjia@ttt>
2024-11-28 15:40:27 +0800

    fix speech sdk issue

--- src/main/resources/static/scripts/audio.js
+++ src/main/resources/static/scripts/audio.js
@@ -4,8 +4,11 @@
     const { recognizer, initializeRecognizer } = window.chatbot || {};
     
     // Function to toggle recording using Azure Speech SDK
-    window.toggleRecording = function() { // **Attached to window object**
+    function toggleRecording() {
+        const recognizer = window.chatbot.getRecognizer(); // Get current recognizer instance
+
         if (recognizer) {
+            console.log('Recognizer is defined:', recognizer);
             if (recognizer.isRecognizing) {
                 recognizer.stopContinuousRecognitionAsync(() => {
                     console.log('Recognition stopped.');
@@ -17,13 +20,17 @@
             } else {
                 startRecognition();
             }
+        } else {
+            console.warn('Recognizer is undefined.');
         }
     }
 
     // Function to start recognition
     function startRecognition() {
+        const recognizer = window.chatbot.getRecognizer();
         if (!recognizer) {
             initializeRecognizer();
+            recognizer = window.chatbot.getRecognizer();
         }
         recognizer.startContinuousRecognitionAsync(
             () => {
